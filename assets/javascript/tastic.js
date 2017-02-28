62afb676f97f1613836f5850873025ad0da813ea
Leigh Fisher <leighfisher@Jamess-MacBook-Air-2.local>
2017-02-27 21:57:43 -0600

    i love bootstrap buttons

---
+++ assets/javascript/tastic.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+$("button").on("click", function() {
+      var character = $(this).attr("data-character");
+      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+        character + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+      $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+        .done(function(response) {
+          var results = response.data;
+
+          for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div class='item'>");
+
+            var rating = results[i].rating;
+
+            var p = $("<p>").text("Rating: " + rating);
+
+            var characterImage = $("<img>");
+            characterImage.attr("src", results[i].images.fixed_height.url);
+
+            gifDiv.prepend(p);
+            gifDiv.prepend(characterImage);
+
+            $("#dc").prepend(gifDiv);
+          }
+        });
+    });
+
+});
